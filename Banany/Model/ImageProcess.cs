c35de0a07b3d1dc148027d9e5731b60a9f5cab40
soziele <sonia.ziele@gmail.com>
2020-05-24 20:53:04 +0200

    Initial commit

---
+++ Banany/Model/ImageProcess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banany
+{
+    class ImageProcess
+    {
+
+        public void LoadFromFile()
+        {
+
+        }
+        public void GetPixels()
+        {
+
+        }
+        public void GetDarkSpots()
+        {
+
+        }
+
+    }
+}
